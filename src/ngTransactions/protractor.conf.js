04833bc18fbf7474e56233bf910d58b79271c2f0
Quinntyne Brown <quinntynebrown@gmail.com>
2017-05-15 08:16:53 -0400

    e2e

--- src/ngTransactions/protractor.conf.js
+++ src/ngTransactions/protractor.conf.js
@@ -3,19 +3,18 @@
     allScriptsTimeout: 11000,
 
     specs: [
-        'dist/e2e.tests.js'
+        'dist/e2e-tests.js'
     ],
 
     capabilities: {
         'browserName': 'chrome'
     },
 
-    baseUrl: 'http://transaction-service.azurewebsites.net/',
+    baseUrl: 'http://transaction-service.azurewebsites.net',
 
     framework: 'jasmine',
 
     jasmineNodeOpts: {
         defaultTimeoutInterval: 30000
     }
-
 };
--- src/ngTransactions/src/app/transactions/transactions.component.e2e-spec.ts
+++ src/ngTransactions/src/app/transactions/transactions.component.e2e-spec.ts
@@ -1,13 +1,10 @@
-﻿//import { browser, element, by } from 'protractor';
+﻿declare var browser, element, by;
 
-describe('Transactions', () => {
-    
-    //beforeEach(async () => {
-    //    return await browser.get('/');
-    //});
-
-    //it('should have correct feature heading', async () => {        
-    //    expect(await element(by.css('h1')).getText()).toEqual('Spend');
-    //});
-
+describe('Transactions', () => {    
+    it('should have correct feature heading',() => {
+        browser.get('');                
+        var headingElement = element(by.css('h1'));
+        expect(headingElement).toBeDefined();
+        expect('SPEND').toEqual(headingElement.getText());       
+    });
 });
